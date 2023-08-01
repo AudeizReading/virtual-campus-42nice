35ad75b296a99b80f101bd25689fec864a81c18b
AudeizReading <aaericia@hotmail.fr>
2023-08-01 08:49:35 +0200

    Volume feature + 42 header feature
    Now we can access to a work directory and update codebase or whatever files. The updates are also made into the container (if available directory provided).
    
    Trying to install a node environment but with the node docker image pulled directly from Docker Hub, it erases all tools installed before, so I think I have to manually install it.
    
    Adding alse the 42 headers, but need to improve the script (what if the login is given empty?). Need also to update if the folder is given with a / final (have to remove it), get back the last part of the folder for putting it into the container.

--- configure.sh
+++ configure.sh
@@ -1,10 +1,12 @@
 #/bin/sh
+
 if [ "${1}" = "run" ]
 then
 	docker restart virtual-campus-42nice
 	docker attach virtual-campus-42nice
 	exit 0
 fi 
+
 if [ `uname ` = "Darwin" ]; then \
 	xhost +localhost; \
 fi 
@@ -28,6 +30,17 @@ RUN apt -y upgrade \\
 EOF
 fi
 
+#read -n 1 -p "Do you need Node.js ? [y/n]: " answer
+#printf "\n"
+#
+#if [ "${answer}" = "y" ]
+#then
+#cat >> ${DOCKERFILE} << EOF
+#FROM node:latest
+#  
+#EOF
+#fi
+
 if [ "${1}" = "defense" ]
 then
 	read -p "Provide the repo's url: " repo
@@ -38,17 +51,40 @@ then
 	cat >> ${DOCKERFILE} << EOF
 WORKDIR /tmp/corrections
 COPY ./corrections .
+
 EOF
 elif [ "${1}" = "dev" ]
 then
-	read -p "Provide the relative path of the work that you need for dev (let empty if no need): " path_work
+	read -n 1 -p "Do you need 42 header ? [y/n] : " need_header
+	printf "\n"
+	if [ "${need_header}" = "y" ]
+	then
+		read -p "Enter your 42 login: " login42
+		printf "\n"
+		cat >> ${DOCKERFILE} << EOF
+ENV USER=${login42}
+ENV MAIL=${login42}@student-42nice.fr
+
+WORKDIR /etc/vim/plugin
+
+RUN git clone https://github.com/42Paris/42header.git header \\
+	&& cp header/plugin/stdheader.vim ./stdheader.vim \\
+	&& rm -rf header \\
+	&& apt remove -y git \\
+	&& apt autoremove -y
+EOF
+	fi
+
+	read -p "Enter the absolute path of the folder you need to access in real-time. Leave blank if you do not need the feature: " path_work
+	printf "\n"
+	eval path_work="${path_work}" # Sinon ca n'expand pas les ~ et $HOME
 	if [ -n "${path_work}" ] && [ -d "${path_work}" ] || [ -f "${path_work}" ]
 	then
-		mkdir -p dev 
-		cp -R ${path_work} ./dev/
+		# recup dernier troncon du path
+		FOLDER=`echo ${path_work} | rev | cut -f 1 -d / | rev`
+		DEVPATH_="-v ${path_work}:/tmp/dev/${FOLDER}"
 		cat >> ${DOCKERFILE} << EOF
-WORKDIR /tmp/dev
-COPY ./dev/ .
+WORKDIR /tmp/dev/${FOLDER}
 EOF
 	fi
 fi
@@ -61,6 +97,7 @@ if [ "${1}" = "defense" ]
 then
 	make launch NAME=virtual-defense-42nice
 	make destroy
-else
-	make launch NAME=virtual-campus-42nice
+elif [ "${1}" = "dev" ]
+then
+	make launch NAME=virtual-campus-42nice DEVPATH="${DEVPATH_}"
 fi
